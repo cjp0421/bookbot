475a5124d0c25aca69fbdcbd6003b6288f5e7347
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2025-01-26 16:04:25 -0600

    refactored main so that helper functions are defined before main to better follow python conventions

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-def main ():
-    file_contents = read_and_print_file("books/frankenstein.txt")
-    num_words = word_count(file_contents)
-    print("Number of words: ", num_words)
-
 def read_and_print_file (file):
     with open(file) as f:
         file_contents = f.read()
@@ -12,5 +7,11 @@ def read_and_print_file (file):
 def word_count (book):
     words = book.split()
     return len(words)
+
+def main ():
+    file_contents = read_and_print_file("books/frankenstein.txt")
+    num_words = word_count(file_contents)
+    print("Number of words: ", num_words)
+
 
 main()
