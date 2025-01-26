120bd4adfb1d5f8366df9f448c5261cc0348945c
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2025-01-26 16:03:00 -0600

    refactored main to put reading and printing file in a function

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-
-
 def main ():
-    with open("books/frankenstein.txt") as f:
+    file_contents = read_and_print_file("books/frankenstein.txt")
+    num_words = word_count(file_contents)
+    print("Number of words: ", num_words)
+
+def read_and_print_file (file):
+    with open(file) as f:
         file_contents = f.read()
         print(file_contents)
-        num_words = word_count(file_contents)
-        print("Number of words: ", num_words)
+    return file_contents
+
 def word_count (book):
     words = book.split()
     return len(words)
