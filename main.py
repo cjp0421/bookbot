fe67d3526a27fdc0881fa649c423195bbc12618c
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2025-01-26 17:39:14 -0600

    cleaned up unnecessary print statements and created a console report function to neatly print info about word count and letter counts

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def read_and_print_file (file):
     with open(file) as f:
         file_contents = f.read()
-        print(file_contents)
+        # print(file_contents)
     return file_contents
 
 def word_count (book):
@@ -10,7 +10,7 @@ def word_count (book):
 
 def character_count (book):
     characters_string = book.lower()
-    print(characters_string)
+    # print(characters_string)
     characters_count = {}
     for character in characters_string:
         if character in characters_count:
@@ -19,12 +19,18 @@ def character_count (book):
             characters_count[character] = 1
     return characters_count
 
+def create_report (file, file_path):
+    num_words = word_count(file)
+    characters_count = character_count(file)
+    alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    print(f"Begin Report on {file_path}:\nNumber of words: {num_words}\n",
+          )
+    for character in characters_count:    
+        if character in alphabet:
+            print(f"{character} is in the book {characters_count[character]} times." )
 
 def main ():
     file_contents = read_and_print_file("books/frankenstein.txt")
-    num_words = word_count(file_contents)
-    print("Number of words: ", num_words)
-    print(character_count(file_contents))
-
+    create_report(file_contents, "books/frankenstein.txt")
 
 main()
