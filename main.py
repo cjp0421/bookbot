52d6a6bcfe4197fcd64cbcd5cc4349ae805965ed
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2025-01-26 17:08:09 -0600

    added unique character counter

--- main.py
+++ main.py
@@ -8,10 +8,23 @@ def word_count (book):
     words = book.split()
     return len(words)
 
+def character_count (book):
+    characters_string = book.lower()
+    print(characters_string)
+    characters_count = {}
+    for character in characters_string:
+        if character in characters_count:
+            characters_count[character]+=1
+        else:
+            characters_count[character] = 1
+    return characters_count
+
+
 def main ():
     file_contents = read_and_print_file("books/frankenstein.txt")
     num_words = word_count(file_contents)
     print("Number of words: ", num_words)
+    print(character_count(file_contents))
 
 
 main()
